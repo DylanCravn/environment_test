d758e67a67046897eebb825feedd86f48446ca8f
DylanCravn <dylancraven@live.com>
2022-09-20 02:13:49 +0000

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Book.create([{ title: 'Bible', author: 'Jesus', price: 0.99, published_date: '2016-01-13'}, 
+             { title: 'Lord of the Flies', author: 'Moses Ingram', price: 11.99, published_date: '2016-05-13'},
+             { title: 'Great Gatsby', author: 'John Doe', price: 5.99, published_date: '1904-01-13'},
+             { title: 'The Lion the Witch and the Wardobe', author: 'Marry Poppins', price: 15, published_date: '2001-01-16'},
+            ])
